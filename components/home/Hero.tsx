60c27c3ac75d1199e559b80d810f0d24fd75a816
Sameh El Zankalony <zankalony@gmail.com>
2024-10-27 18:05:35 +0300

    spelling correction
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
       <div>
         <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
-          We ae changing the way people shop
+          We are changing the way people shop
         </h1>
         <p className="mt-8 max-w-xl text-lg - leading-8 text-muted-foreground">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe illo
