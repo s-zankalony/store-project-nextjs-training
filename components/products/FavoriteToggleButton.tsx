3cd5aa0a272dcf3b43b90ef6ad0c0f590a43221a
Sameh El Zankalony <zankalony@gmail.com>
2024-10-27 18:11:06 +0300

    Refactor FavoriteToggleButton component and add console log for productId
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,10 +2,10 @@ import { FaHeart } from 'react-icons/fa';
 import { Button } from '../ui/button';
 
 const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  console.log(productId);
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
-      {productId}
     </Button>
   );
 };
