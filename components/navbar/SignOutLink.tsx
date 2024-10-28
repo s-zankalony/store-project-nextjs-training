72a9e0793c4cf5944634be209ce5f698a221883c
Sameh El Zankalony <zankalony@gmail.com>
2024-10-28 18:03:29 +0300

    - Refactor dependencies and add Toaster component - Adding Clerk Auth Manager - Complete the Navbar dropdown menu with login/logout
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,4 +1,20 @@
+'use client';
+
+import { useToast } from '@/hooks/use-toast';
+import { SignOutButton } from '@clerk/nextjs';
+import Link from 'next/link';
+
 const SignOutLink = () => {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: 'Logout successful' });
+  };
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 };
 export default SignOutLink;
