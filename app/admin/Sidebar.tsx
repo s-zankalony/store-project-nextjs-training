60fe4b5b007d5ea708a7cb34a4ad65ceac7f3892
Sameh El Zankalony <zankalony@gmail.com>
2024-11-04 16:36:25 +0200

    Implement EditProductPage with form handling and image update functionality; add ImageInputContainer component
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -17,10 +17,9 @@ const Sidebar = () => {
             asChild
             className="w-full mb-2 capitalize font-normal justify-start"
             variant={variant}
+            key={link.href}
           >
-            <Link key={link.href} href={link.href}>
-              {link.label}
-            </Link>
+            <Link href={link.href}>{link.label}</Link>
           </Button>
         );
       })}
