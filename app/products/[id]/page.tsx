c0b0be971ef79d13dac6e4af622cffe0c240b041
Sameh El Zankalony <zankalony@gmail.com>
2024-11-14 15:27:42 +0200

    Implement reviews functionality: add review deletion, loading states, and conditional rendering for review submission; update actions for fetching and finding user reviews
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 import BreadCrumbs from '@/components/single-product/BreadCrumbs';
-import { fetchSingleProduct } from '@/utils/actions';
+import { fetchSingleProduct, findExistingReview } from '@/utils/actions';
 import Image from 'next/image';
 import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
@@ -8,12 +8,15 @@ import ProductRating from '@/components/single-product/ProductRating';
 import ShareButton from '@/components/single-product/ShareButton';
 import SubmitReview from '@/components/reviews/SubmitReview';
 import ProductReviews from '@/components/reviews/ProductReviews';
+import { auth } from '@clerk/nextjs/server';
 
 const SingleProductPage = async ({ params }: { params: { id: string } }) => {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
-
+  const { userId } = await auth();
+  const reviewDoesNotExist =
+    userId && !(await findExistingReview(userId, product.id));
   return (
     <section>
       <BreadCrumbs name={name} />
@@ -48,7 +51,7 @@ const SingleProductPage = async ({ params }: { params: { id: string } }) => {
         </div>
       </div>
       <ProductReviews productId={params.id} />
-      <SubmitReview productId={params.id} />
+      {reviewDoesNotExist && <SubmitReview productId={params.id} />}
     </section>
   );
 };
